68a3356f4f078cec80fe49123860200e7d0aeefe
이진우 <39187855+pyrosoda@users.noreply.github.com>
2024-09-07 20:57:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
-import { FBXLoader } from './lib/FBXLoader.js';
+import * as THREE from 'three';
+import { FBXLoader } from 'FBXLoader';
 
 
 const scene = new THREE.Scene();
