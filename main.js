e3d013e6cc4a09b905968bd60cd4b7adaf29b898
이진우 <39187855+pyrosoda@users.noreply.github.com>
2024-09-07 18:03:22 +0900

    Update main.js
    cube debug

--- main.js
+++ main.js
@@ -59,8 +59,6 @@ document.body.appendChild(renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
